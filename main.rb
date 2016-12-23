cafb3cf848c7743974a64202f8db7ac3154ac2c1
Artem Gurzhii <artem.gurzhii@gmail.com>
2016-12-23 19:30:07 +0200

    initial commit

---
+++ main.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+
+require 'openssl'
+require 'nokogiri'
+require 'net/http'
+require 'nokogiri'
+require 'fileutils'
+
+def compare_output
+	begin
+		x = FileUtils.compare_file('index.html', 'current.html')
+		puts x
+	rescue => err
+    puts "Exception: #{err}"
+    err
+	end
+end
+
+def request
+	uri = URI('https://wis.fit.vutbr.cz/FIT/st/course-reg.php.cs')
+	Net::HTTP.start(uri.host, uri.port,
+	  :use_ssl => uri.scheme == 'https',
+	  :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
+
+	  request = Net::HTTP::Get.new uri.request_uri
+	  request.basic_auth 'xhurzh00', 'pumduj6are'
+	  response = http.request request
+		# puts response.body
+		File.write('current.html', response.body)
+		# unless compare_output()
+		# 	send_message('')
+		# end
+	end
+end
+every 1.minute do
+	runner "MyModel.some_process"
+	rake "my:rake:task"
+	command "/usr/bin/my_great_command"
+end
+
+request()
