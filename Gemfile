f22f97adbb3b4dc5a0ca7c9051fb426478fc60d6
Artem Gurzhii <artem.gurzhii@gmail.com>
2016-12-25 23:10:36 +0200

    fixing

--- Gemfile
+++ Gemfile
@@ -2,4 +2,5 @@ source 'https://rubygems.org'
 
 gem 'telegram-bot-ruby'
 gem 'activerecord'
+gem 'pg'
 gem 'rake'
--- lib/app_configurator.rb
+++ lib/app_configurator.rb
@@ -1,12 +1,22 @@
 require 'logger'
+require './lib/database_connector'
 require './lib/yaml_parser'
 
 class AppConfigurator
-  def get_token
-    Configurations::TELEGRAM_TOKEN
-  end
+	def configure
+		setup_database
+	end
+
+	def get_token
+		Configurations::TELEGRAM_TOKEN
+	end
 
   def get_logger
     Logger.new(STDOUT, Logger::DEBUG)
   end
+
+	private
+		def setup_database
+			DatabaseConnector.establish_connection
+		end
 end
